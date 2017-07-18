aa7945f3a482918032eae7544b80f76941709a3f
Alex Glover <alexanderbglover@gmail.com>
2017-07-18 16:33:38 -0400

    Added proper link paths

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("a").on('click', function(event) {
+  $("nav a").on('click', function(event) {
     if (this.hash !== "10") {
       event.preventDefault();
       var hash = this.hash;
